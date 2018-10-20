9d13fb9c6100631c8b578a8452c718015b77d2ef
Roman <rozario91208@gmail.com>
2018-10-21 00:33:03 +0300

    pipe -  date | uppercase

--- angular-theory/src/app/cars/cars.component.ts
+++ angular-theory/src/app/cars/cars.component.ts
@@ -10,6 +10,13 @@ export class CarsComponent {
   carName = '';
   addCarStatus = false;
   cars = ['Ford', 'Audi', 'BMW', 'Mazda', 'Lada', 'Bently'];
+  dates = [
+    new Date(2015, 3, 1).toDateString(),
+    new Date(2011, 1, 6).toDateString(),
+    new Date(2007, 3, 4).toDateString(),
+    new Date(2009, 2, 2).toDateString(),
+    new Date(2017, 9, 4).toDateString()
+  ];
 
   constructor() {}
 
@@ -17,10 +24,6 @@ export class CarsComponent {
     this.addCarStatus = true;
     this.cars.push(this.carName);
     this.carName = '';
-  }
-
-  setBigCarText(car: string){
-    return car.length > 4 ? true : false;
   }
 
 }
