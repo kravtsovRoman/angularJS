e717273353e88ff1fb06aca293e0be6e9fe9bb0b
Roman <rozario91208@gmail.com>
2018-10-21 17:08:02 +0300

    create directive

--- angular-theory/src/app/app.module.ts
+++ angular-theory/src/app/app.module.ts
@@ -3,22 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-import {FormsModule} from "@angular/forms";
-import { AddCarComponent } from './add-car/add-car.component';
+import { BackgroundDirective } from './directives/background.directive';
+import { MyDirectiveDirective } from './directives/my-directive.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent,
-    AddCarComponent
+    BackgroundDirective,
+    MyDirectiveDirective
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
