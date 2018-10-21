e71bb510c5b68165dd299a6788ef84c33124b25b
Roman <rozario91208@gmail.com>
2018-10-21 15:31:45 +0300

    HTML to component

--- angular-theory/src/app/car/car.component.ts
+++ angular-theory/src/app/car/car.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import {Component, ContentChild, ElementRef, Input} from '@angular/core';
 
 @Component({
   selector: 'app-car',
@@ -8,5 +8,10 @@ import { Component, Input } from '@angular/core';
 export class CarComponent {
 
   @Input('carItem') car: {name: string, year: number};
+  @ContentChild('carHeading') carHeading: ElementRef;
 
+  ngAfterViewInit(){
+    console.log(this.carHeading);
+  }
 }
+
